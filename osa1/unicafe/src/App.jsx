ab50a0c535962b863c9ca81c4b5a5bd2896e0742
Markus Aurala <markus.aurala@gmail.com>
2025-02-08 20:44:25 +0200

    unicafe, step1b

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,14 +1,36 @@
 import { useState } from 'react'
 
+const Header = ({ text }) => <h1>{text}</h1>
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>{text}</button>
+)
+
+const Label = ({ text, value }) => (
+  <p>
+    {text} {value}
+  </p>
+)
+
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   return (
     <div>
-      code here
+      <Header text="give feedback" />
+
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+
+      <Header text="statistics" />
+
+      <Label text="good" value={good} />
+      <Label text="neutral" value={neutral} />
+      <Label text="bad" value={bad} />
+
     </div>
   )
 }
