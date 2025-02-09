6c5f824c7803f4c239b591bd3141e2d27ef9ba6d
Markus Aurala <markus.aurala@gmail.com>
2025-02-09 16:13:13 +0200

    puhelinluettelo, step2

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -8,6 +8,13 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const isExistingPerson = (name) => {
+    const exists = persons.some(person => person.name === name)
+    console.log("isExistingPerson:", name, exists)
+    if (exists) {alert(`${newName} is already added to the phonebook`)}
+    return exists
+  }
+
   const handlePersonChange = (event) => {
     console.log("handlePersonChange:", event.target.value)
     setNewName(event.target.value)
@@ -17,9 +24,11 @@ const App = () => {
     event.preventDefault()
     console.log('Button clicked', event.target)
     const personObject = { name: newName }
-    console.log('Adding person:', personObject)
-    setPersons(persons.concat(personObject))
-    setNewName('')
+    if (!isExistingPerson(newName)) {
+      console.log('Adding person:', personObject)
+      setPersons(persons.concat(personObject))
+      setNewName('')
+    }
   }
 
   return (
