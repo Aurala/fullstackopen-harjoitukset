ec74b925c9c4423e5925dcba98976f7c7a56dfe2
Markus Aurala <markus.aurala@gmail.com>
2025-04-02 16:42:13 +0300

    apufunktioita ja yksikkötestejä, step2

---
+++ osa4/blogilista/tests/list_helper.test.js
@@ -0,0 +1,59 @@
+const { describe, test } = require('node:test');
+const assert = require('node:assert/strict');
+const listHelper = require('../utils/list_helper.js')
+
+describe('total likes', () => {
+ 
+  test('when list has no blogs, sum equals to zero', () => {
+    const blogs = []
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog, sum equals to the likes of that', () => {
+    const blogs = [
+      {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0
+      }
+    ]
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 7)
+  })
+
+  test('when list has many blogs, sum equals to the likes of all', () => {
+    const blogs = [
+      {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0
+      },
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0
+      },
+      {
+        _id: "5a422b3a1b54a676234d17f9",
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+        __v: 0
+      }
+    ]
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 24)
+  })
+
+})
