2ed2f01278063eb181aead41243f39354e70a4de
Markus Aurala <markus.aurala@gmail.com>
2025-04-06 16:55:31 +0300

    blogilistan laajennus, step6

--- osa4/blogilista/utils/config.js
+++ osa4/blogilista/utils/config.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 let PORT = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
+let SECRET = process.env.SECRET
 
 // Not strictly required here because the tests initialize a MongoDB memory server
 if (process.env.NODE_ENV === 'test') {
@@ -10,5 +11,6 @@ if (process.env.NODE_ENV === 'test') {
 
 module.exports = {
   MONGODB_URI,
-  PORT
+  PORT,
+  SECRET
 }
